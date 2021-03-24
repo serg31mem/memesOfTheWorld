c2b68b4512e15cb97fda5fa756525cebeaa866b3
Serg <serg31karpov@gmail.com>
2021-03-24 14:53:13 +0400

    refactoring the nav bar

--- src/Components/NavBar/Nav.js
+++ src/Components/NavBar/Nav.js
@@ -2,31 +2,29 @@ import s from './Nav.module.css'
 import {NavLink} from "react-router-dom";
 import BlockFriends from "./BlockFriends/BlockFriends";
 
+const linkCreator = (to, linkName) => {
+    return <div className={s.item}>
+        <NavLink to={`/${to}`} activeClassName={s.activeLink}>{linkName}</NavLink>
+    </div>
+}
+
 const Nav = (props) => {
-    debugger
     return (
         <nav className={s.nav}>
-            <div className={s.item}>
-                <NavLink to='/profile' activeClassName={s.activeLink}>Profile</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to='/friends' activeClassName={s.activeLink}>Friends</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to='/dialogs' activeClassName={s.activeLink}>Messages</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to='/news' activeClassName={s.activeLink}>News</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to='/users' activeClassName={s.activeLink}>Users</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to='/music' activeClassName={s.activeLink}>Music</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to='/settings' activeClassName={s.activeLink}>Settings</NavLink>
-            </div>
+            {linkCreator('profile', 'Profile')}
+
+            {linkCreator('friends', 'Friends')}
+
+            {linkCreator('dialogs', 'Messages')}
+
+            {linkCreator('news', 'News')}
+
+            {linkCreator('users', 'Users')}
+
+            {linkCreator('music', 'Music')}
+
+            {linkCreator('settings', 'Settings')}
+
             <div>
                 <div className={s.itemFriends}>
                     Friends
